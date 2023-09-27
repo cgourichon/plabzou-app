c441f914d517c2d919c0e9088719ddc89a60fa6d
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-09-27 22:20:05 +0200

    Add auth store

---
+++ src/stores/auth.store.js
@@ -0,0 +1,48 @@
+import {defineStore} from "pinia";
+import axiosClient from "@/axios";
+
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        token: localStorage.getItem('token') || null,
+        tokenExpiration: localStorage.getItem('tokenExpiration') || null,
+        authenticatedUser: null
+    }),
+    getters: {
+        isAuthenticated() {
+            const now = Math.floor(Date.now() / 1000)
+
+            if (now > this.tokenExpiration) {
+                this.clearToken()
+
+                return false
+            }
+
+            return this.token !== null
+        }
+    },
+    actions: {
+        async login(credentials) {
+            const response = await axiosClient.post('/auth/login', credentials)
+            this.setToken(response.data.data.token, response.data.data.expires_at)
+        },
+        async fetchAuthenticatedUser() {
+            const response = await axiosClient.get('/auth/me')
+            this.authenticatedUser = response.data.data
+        },
+        setToken(token, tokenExpiration) {
+            this.token = token
+            this.tokenExpiration = tokenExpiration
+
+            localStorage.setItem('token', token)
+            localStorage.setItem('tokenExpiration', tokenExpiration)
+        },
+        clearToken() {
+            this.token = null
+            this.tokenExpiration = null
+            this.authenticatedUser = null
+
+            localStorage.removeItem('token')
+            localStorage.removeItem('tokenExpiration')
+        }
+    }
+})
