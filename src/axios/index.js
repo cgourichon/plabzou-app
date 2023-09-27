3e0bd9e88829a6e1d7cbee37b6e95fd4933a574d
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-09-27 22:19:52 +0200

    Add axios global config

---
+++ src/axios/index.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import {useAuthStore} from "@/stores/auth.store";
+
+const axiosClient = axios.create({
+    baseURL: 'http://plabzou-api.test/api'
+})
+
+axiosClient.interceptors.request.use(config => {
+    const authStore = useAuthStore()
+
+    if (authStore.isAuthenticated) {
+        config.headers['Authorization'] = authStore.token
+    }
+
+    return config
+})
+
+export default axiosClient
