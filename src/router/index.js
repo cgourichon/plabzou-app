3bcd626ad86cf41269af4471699a766857bcb277
Marjo <marjorie.gestin2020@campus-eni.fr>
2023-10-18 00:39:39 +0200

    add route

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import LoginView from "@/views/LoginView.vue";
 import UserListView from "@/views/Management/User/UserListView.vue";
 import UserCreateView from "@/views/Management/User/UserCreateView.vue";
 import UserEditView from "@/views/Management/User/UserEditView.vue";
+import MessagesView from "@/views/MessagesView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,11 @@ const router = createRouter({
             path: '/connexion',
             name: 'login',
             component: LoginView
+        },
+        {
+          path: '/messagerie',
+          name: '/messagerie',
+          component: MessagesView
         },
         {
             path: '/gestion/utilisateurs',
