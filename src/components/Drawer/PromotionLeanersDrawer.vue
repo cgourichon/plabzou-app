4747cda8bc0144967bf6ab0bbd463e0c7da1e8ac
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-10-18 21:31:34 +0200

    Show promotion learners in drawer

--- src/components/Drawer/PromotionLeanersDrawer.vue
+++ src/components/Drawer/PromotionLeanersDrawer.vue
@@ -1,39 +1,33 @@
 <script setup>
-import {ref} from "vue";
-
-const props = defineProps({
+defineProps({
   promotion: {
     type: Object,
     default: null,
   }
 })
-
-const isOpen = ref(true)
-
-const toggle = () => isOpen.value = !isOpen.value
-const close = function() {
-  toggle()
-}
-
 </script>
 
 <template>
   <nord-layout>
-    <nord-drawer slot="drawer" v-if="isOpen">
+    <nord-drawer slot="drawer">
       <nord-header slot="header">
-        <h3 class="n-typescale-l n-truncate">Promo. {{ promotion?.name }} </h3>
-        <nord-button slot="end" @click="close" id="close" variant="plain" aria-describedby="close-sidebar" size="s">
+        <h3 class="n-typescale-l n-truncate">{{ promotion?.name }} </h3>
+        <nord-button id="close" slot="end" aria-describedby="close-sidebar" size="s" variant="plain">
           <nord-icon name="interface-close" size="s"></nord-icon>
         </nord-button>
         <nord-tooltip id="close-sidebar">Close</nord-tooltip>
       </nord-header>
 
-
       <div class="n-typeset">
         <nord-stack direction="vertical">
-          <nord-stack direction="horizontal" align-items="center" v-for="learner in promotion?.learners" :key="learner.id">
+          <nord-stack
+              v-for="learner in promotion?.learners"
+              :key="learner.id"
+              align-items="center"
+              direction="horizontal"
+          >
             <nord-avatar size="l"/>
-            <div>{{ learner?.full_name }} </div>
+            {{ learner?.full_name }}
           </nord-stack>
         </nord-stack>
       </div>
