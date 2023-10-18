b6d83c8c78db7045c3bfcd07a43e4e533762c810
corentinho <corentin.gourichon@proton.me>
2023-10-18 13:40:03 +0200

    PBZ-91 Ajout de la liste des apprenants

--- src/views/Management/Promotion/PromotionListView.vue
+++ src/views/Management/Promotion/PromotionListView.vue
@@ -1,7 +1,8 @@
 <script setup>
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import {usePromotionStore} from "@/stores/promotion.store";
 import {getFrenchDate} from "@/utils/date"
+import PromotionLeanersDrawer from "@/components/Drawer/PromotionLeanersDrawer.vue";
 
 const promotionStore = usePromotionStore()
 promotionStore.resetPromotions()
@@ -10,13 +11,21 @@ onMounted(async () => {
   await promotionStore.fetchPromotions()
 })
 
-const learnersLabel = (learnersNumber) => learnersNumber > 1 ? 'apprenants' : 'apprenant'
+const drawer = ref(null)
+const selectedPromotion = ref({})
+function selectPromotion(promotion) {
+  selectedPromotion.value = promotion
+  drawer.value.toggle()
+}
+
+const getLearnersLabel = (learners) => learners.length > 1 ? 'apprenants' : 'apprenant'
 </script>
 
 <template>
   <nord-card>
-    <h2 slot="header">Liste des promotions</h2>
+    {{ this.selectedPromotion }}
 
+    <h2 slot="header">Liste des promotions</h2>
     <nord-table striped>
       <table>
         <thead>
@@ -25,7 +34,8 @@ const learnersLabel = (learnersNumber) => learnersNumber > 1 ? 'apprenants' : 'a
           <th>Nom</th>
           <th>Cursus suivi</th>
           <th>Période de formation</th>
-          <th>Campus de rattachement</th>
+          <th>Effectif</th>
+          <th>Ville de rattachement</th>
           <th class="n-table-align-right">Actions</th>
         </tr>
         </thead>
@@ -43,16 +53,26 @@ const learnersLabel = (learnersNumber) => learnersNumber > 1 ? 'apprenants' : 'a
           <td>
             {{ getFrenchDate(promotion.starts_at) }} - {{ getFrenchDate(promotion.ends_at) }}
           </td>
+          <td class="n-table-align-left">
+            <nord-stack direction="horizontal" justify-content="space-around" align-items="center" gap="s">
+              {{ promotion.learners.length }} {{ getLearnersLabel(promotion.learners)}}
+              <nord-button @click="selectPromotion(promotion)" size="s" variant="primary">
+                <nord-icon slot="start" name="user-multiple"/>
+              </nord-button>
+            </nord-stack>
+          </td>
           <td>
-            {{ promotion?.city.name }}
+            {{ promotion?.city?.name ?? '-'}}
           </td>
           <td class="n-table-align-right">
+
           </td>
         </tr>
         </tbody>
       </table>
     </nord-table>
   </nord-card>
+  <PromotionLeanersDrawer ref="drawer" :promotion="selectedPromotion"/>
 </template>
 
 <style scoped>
