9c1ddb717fcd14751a270c91dc163a9bd7af5ca7
Marjo <marjorie.gestin2020@campus-eni.fr>
2023-10-18 13:50:00 +0200

    update nav

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -9,96 +9,96 @@ const authStore = useAuthStore()
 const authenticatedUser = computed(() => authStore.authenticatedUser)
 
 const logout = async () => {
-  await authStore.logout()
-  await router.push('/connexion')
+    await authStore.logout()
+    await router.push('/connexion')
 }
 </script>
 
 <template>
-  <nord-navigation slot="nav">
-    <nord-dropdown slot="header" expand>
-      <nord-button slot="toggle" expand>
-        <nord-avatar slot="start" name="Plabzou" variant="square">P</nord-avatar>
-        Plabzou
-      </nord-button>
+    <nord-navigation slot="nav">
+        <nord-dropdown slot="header" expand>
+            <nord-button slot="toggle" expand>
+                <nord-avatar slot="start" name="Plabzou" variant="square">P</nord-avatar>
+                Plabzou
+            </nord-button>
 
-      <nord-dropdown-group heading="Applications">
-        <nord-dropdown-item>
-          <nord-avatar slot="start" name="Plabzou" size="s" variant="square">P</nord-avatar>
-          Plabzou
-          <nord-icon slot="end" name="interface-checked"></nord-icon>
-        </nord-dropdown-item>
-      </nord-dropdown-group>
-    </nord-dropdown>
+            <nord-dropdown-group heading="Applications">
+                <nord-dropdown-item>
+                    <nord-avatar slot="start" name="Plabzou" size="s" variant="square">P</nord-avatar>
+                    Plabzou
+                    <nord-icon slot="end" name="interface-checked"></nord-icon>
+                </nord-dropdown-item>
+            </nord-dropdown-group>
+        </nord-dropdown>
 
-    <nord-nav-group heading="Global">
-      <RouterLink to="/">
-        <nord-nav-item :active="$route.path === '/'" icon="interface-home">
-          Accueil
-        </nord-nav-item>
-      </RouterLink>
+        <nord-nav-group heading="Global">
+            <RouterLink to="/">
+                <nord-nav-item :active="$route.path === '/'" icon="interface-home">
+                    Accueil
+                </nord-nav-item>
+            </RouterLink>
 
-      <RouterLink to="/about">
-        <nord-nav-item :active="$route.path === '/about'" icon="interface-info">
-          A propos
-        </nord-nav-item>
-      </RouterLink>
-    </nord-nav-group>
+            <RouterLink to="/about">
+                <nord-nav-item :active="$route.path === '/about'" icon="interface-info">
+                    A propos
+                </nord-nav-item>
+            </RouterLink>
+        </nord-nav-group>
 
-    <nord-nav-group>
-      <RouterLink to="/messagerie">
-        <nord-nav-item :active="$route.path === '/messagerie'">
-          Messages
-        </nord-nav-item>
-      </RouterLink>
-    </nord-nav-group>
+        <nord-nav-group heading="Gestion">
+            <RouterLink to="/gestion/utilisateurs">
+                <nord-nav-item :active="$route.path.startsWith('/gestion/utilisateurs')" icon="user-multiple">
+                    Utilisateurs
+                </nord-nav-item>
+            </RouterLink>
 
-    <nord-nav-group heading="Gestion">
-      <RouterLink to="/gestion/utilisateurs">
-        <nord-nav-item :active="$route.path.startsWith('/gestion/utilisateurs')" icon="user-multiple">
-          Utilisateurs
-        </nord-nav-item>
-      </RouterLink>
+            <RouterLink to="/gestion/categories">
+                <nord-nav-item :active="$route.path.startsWith('/gestion/categories')" icon="interface-grid">
+                    Catégories
+                </nord-nav-item>
+            </RouterLink>
 
-      <RouterLink to="/gestion/categories">
-        <nord-nav-item :active="$route.path.startsWith('/gestion/categories')" icon="interface-grid">
-          Catégories
-        </nord-nav-item>
-      </RouterLink>
+            <RouterLink to="/gestion/cursus">
+                <nord-nav-item :active="$route.path.startsWith('/gestion/cursus')" icon="interface-grid">
+                    Cursus
+                </nord-nav-item>
+            </RouterLink>
+        </nord-nav-group>
 
-      <RouterLink to="/gestion/cursus">
-        <nord-nav-item :active="$route.path.startsWith('/gestion/cursus')" icon="interface-grid">
-          Cursus
-        </nord-nav-item>
-      </RouterLink>
-    </nord-nav-group>
+        <nord-nav-group heading="Communication">
+            <RouterLink to="/messagerie">
+                <nord-nav-item :active="$route.path === '/messagerie'" icon="generic-mail">
+                    Messages
+                </nord-nav-item>
+            </RouterLink>
+        </nord-nav-group>
 
-    <nord-nav-group v-if="authenticatedUser === null" slot="footer" heading="Authentification">
-      <RouterLink to="/connexion">
-        <nord-nav-item :active="$route.path === '/connexion'" icon="interface-login">
-          Connexion
-        </nord-nav-item>
-      </RouterLink>
-    </nord-nav-group>
+        <nord-nav-group v-if="authenticatedUser === null" slot="footer" heading="Authentification">
+            <RouterLink to="/connexion">
+                <nord-nav-item :active="$route.path === '/connexion'" icon="interface-login">
+                    Connexion
+                </nord-nav-item>
+            </RouterLink>
+        </nord-nav-group>
 
-    <nord-dropdown v-else slot="footer" expand>
-      <nord-button slot="toggle" expand>
-        <nord-avatar slot="start" aria-hidden="true" name="Prénom NOM"></nord-avatar>
-        {{ authenticatedUser?.first_name }} {{ authenticatedUser?.last_name }}
-      </nord-button>
+        <nord-dropdown v-else slot="footer" expand>
+            <nord-button slot="toggle" expand>
+                <nord-avatar slot="start" aria-hidden="true" name="Prénom NOM"></nord-avatar>
+                {{ authenticatedUser?.first_name }} {{ authenticatedUser?.last_name }}
+            </nord-button>
 
-      <nord-dropdown-group>
-        <nord-dropdown-item href="#">Voir mon profil</nord-dropdown-item>
-      </nord-dropdown-group>
+            <nord-dropdown-group>
+                <nord-dropdown-item href="#">Voir mon profil</nord-dropdown-item>
+            </nord-dropdown-group>
 
-      <nord-dropdown-group>
-        <nord-dropdown-item>Aide & Support</nord-dropdown-item>
-      </nord-dropdown-group>
+            <nord-dropdown-group>
+                <nord-dropdown-item>Aide & Support</nord-dropdown-item>
+            </nord-dropdown-group>
 
-      <nord-dropdown-item @click.prevent="logout">
-        Déconnexion
-        <nord-icon slot="end" name="interface-logout"></nord-icon>
-      </nord-dropdown-item>
-    </nord-dropdown>
-  </nord-navigation>
+            <nord-dropdown-item @click.prevent="logout">
+                Déconnexion
+                <nord-icon slot="end" name="interface-logout"></nord-icon>
+            </nord-dropdown-item>
+        </nord-dropdown>
+    </nord-navigation>
 </template>
