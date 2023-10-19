9dda040c7c5d393c620c47d9ce5c82486a8d5e00
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-10-19 16:03:42 +0200

    Add custom css for fullcalendar

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import "@nordhealth/icons"
 import "./assets/styles/theme.scss"
 import "vue-multiselect/dist/vue-multiselect.css"
 import "./assets/styles/multiselect.css"
+import "./assets/styles/fullcalendar.css"
 import "ag-grid-community/styles/ag-grid.css";
 import "@nordhealth/ag-theme-nord";
 
