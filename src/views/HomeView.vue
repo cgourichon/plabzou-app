effa821cd3609d28d567b13eaaf6b5f770ca7070
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-10-19 02:25:14 +0200

    Update home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,7 @@
 <script setup>
-import VueMultiselect from 'vue-multiselect'
-import FullCalendar from '@fullcalendar/vue3'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
-
-const selected = null
-const options = ['list', 'of', 'options', 'from', 'the', 'server']
-
-const calendarOptions = {
-  plugins: [ dayGridPlugin, interactionPlugin ],
-  initialView: 'dayGridMonth',
-}
+import MyPlanning from "@/components/Planning/MyPlanning.vue";
 </script>
 
 <template>
-  <h1>Accueil</h1>
-
-  <div>
-    <VueMultiselect
-        v-model="selected"
-        :options="options">
-    </VueMultiselect>
-  </div>
-
-  <div>
-    <FullCalendar :options="calendarOptions" />
-  </div>
+  <MyPlanning />
 </template>
