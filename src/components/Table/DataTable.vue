873ef8c04f108572bfc3a803ac1f92dc8d66e94a
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-10-19 11:59:47 +0200

    Update datatable component

--- src/components/Table/DataTable.vue
+++ src/components/Table/DataTable.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { AgGridVue } from "ag-grid-vue3";
-import {onMounted, reactive, ref} from "vue";
+import {AgGridVue} from "ag-grid-vue3";
+import {reactive, ref} from "vue";
 
 const props = defineProps({
   columns: {
@@ -21,7 +21,7 @@ const gridApi = ref(null) // Optional - for accessing Grid's API
 
 // Obtain API from grid's onGridReady event
 const onGridReady = (params) => {
-  gridApi.value = params.api;
+  gridApi.value = params.api
 }
 
 // Each Column Definition results in one Column.
@@ -35,18 +35,36 @@ const defaultColDef = {
   filter: true,
   flex: 1
 }
+
+const quickSearch = ref('')
+
+const onFilterTextBoxChanged = () => {
+  gridApi.value.setQuickFilter(quickSearch.value);
+}
 </script>
 
 <template>
-  <ag-grid-vue
-      style="height: 500px"
-      class="ag-theme-nord"
-      :columnDefs="columnDefs.value"
-      :rowData="data"
-      :defaultColDef="defaultColDef"
-      :components="{...components}"
-      @grid-ready="onGridReady"
-  />
+  <nord-stack>
+    <nord-input
+        id="quick-search"
+        v-model="quickSearch"
+        hide-label
+        label="Rechercher et filtrer"
+        placeholder="Rechercher et filtrer"
+        type="search"
+        @input="onFilterTextBoxChanged"
+    />
+
+    <ag-grid-vue
+        :columnDefs="columnDefs.value"
+        :components="{...components}"
+        :defaultColDef="defaultColDef"
+        :rowData="data"
+        class="ag-theme-nord"
+        style="height: 500px"
+        @grid-ready="onGridReady"
+    />
+  </nord-stack>
 </template>
 
 <style scoped>
