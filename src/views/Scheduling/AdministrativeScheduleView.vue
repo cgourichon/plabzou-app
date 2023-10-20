65368e6fecfad594cc12fe2a1412ed7feedad637
Arthur Mallet <malleta01@gmail.com>
2023-10-20 18:56:40 +0200

    Récupération de la promotion de base dans la page de planning des promo + affichage du modal d'avancement de la promo sélectionné

--- src/views/Scheduling/AdministrativeScheduleView.vue
+++ src/views/Scheduling/AdministrativeScheduleView.vue
@@ -1,55 +1,66 @@
 <script setup>
-  import {usePromotionStore} from "@/stores/promotion.store";
-  import {computed, onMounted, ref} from "vue";
-  import Calendar from "@/components/Calendar/Calendar.vue";
-  import {useTimeslotStore} from "@/stores/timeslot.store";
+import {usePromotionStore} from "@/stores/promotion.store";
+import {useTimeslotStore} from "@/stores/timeslot.store";
+import PromotionProgress from "@/components/Promotion/PromotionProgress.vue";
+import Calendar from "@/components/Calendar/Calendar.vue";
+import {computed, onMounted, ref} from "vue";
+import {useRoute} from "vue-router";
 
-  const promotionStore = usePromotionStore()
-  const timeslotStore = useTimeslotStore()
+const promotionStore = usePromotionStore()
+const timeslotStore = useTimeslotStore()
 
-  const props = defineProps({
-    id: {
-      type: Number,
-      default: null,
-    },
-  })
+let selectedPromotion = ref(null)
 
-  const selectedPromotion = ref(null)
+onMounted(async () => {
+  if (useRoute().params.id) {
+    console.log(useRoute().params.id)
+    await promotionStore.fetchPromotion(useRoute().params.id, {advancement: 1})
+    selectedPromotion.value = promotionStore.promotion
+  }
+  await promotionStore.fetchPromotions()
 
-  onMounted(async () => {
-    await promotionStore.fetchPromotions()
+  await fetchTimeslots()
+})
 
-    if (props.id) {
-      selectedPromotion.value = await promotionStore.fetchPromotion(props.id)
-    }
+const modalAdvancement = ref(null)
 
-    await fetchTimeslots()
-  })
+const showAdvancement = () => {
+  modalAdvancement.value.showModal()
+}
 
-  const fetchTimeslots = async () => {
-    timeslotStore.resetTimeslots()
+const fetchTimeslots = async () => {
+  timeslotStore.resetTimeslots()
 
-    await timeslotStore.fetchTimeslots()
+  await timeslotStore.fetchTimeslots()
+}
+
+const filteredTimeslots = computed(() => {
+  let timeslots = timeslotStore?.timeslots
+
+  if (selectedPromotion.value) {
+    let filtered = timeslots.filter(timeslot => timeslot.promotions.some(promotion => promotion.id === selectedPromotion.value.id))
+    return filtered.length ? filtered : []
   }
 
-  const filteredTimeslots = computed(() => {
-      let timeslots = timeslotStore?.timeslots
+  return timeslots
+})
 
-      if (selectedPromotion.value) {
-        let filtered = timeslots.filter(timeslot => timeslot.promotions.some(promotion => promotion.id === selectedPromotion.value.id))
-        return filtered.length ? filtered : []
-      }
-
-      return timeslots
-  })
-
-  const hasTimeslots = computed(() => filteredTimeslots?.value?.length)
+const hasTimeslots = computed(() => filteredTimeslots?.value?.length)
 
 </script>
 
 <template>
   <nord-card>
-    <h2 slot="header">Planning d'une promotion</h2>
+    <nord-stack slot="header" class="n-stack-horizontal">
+      <h2 v-if="!selectedPromotion">Planning des promotions</h2>
+
+      <h2 v-else>Planning de la promotion : {{ selectedPromotion.name }}</h2>
+      <nord-button size="s" variant="primary" @click="showAdvancement">
+        <nord-icon slot="start" name="interface-edit-on"/>
+        Avancement
+      </nord-button>
+    </nord-stack>
+
     <nord-stack slot="header-end">
       <multi-select
           v-model="selectedPromotion"
@@ -67,6 +78,10 @@
   </nord-card>
   <Calendar v-if="hasTimeslots" :events="filteredTimeslots" :promotion=selectedPromotion view="dayGridMonth"/>
 
+  <nord-modal v-if="selectedPromotion" :open="false" ref="modalAdvancement" size="l" aria-labelledby="title">
+    <h2 slot="header" id="title">Promotion : {{ selectedPromotion.name }}</h2>
+    <promotion-progress :promotion="selectedPromotion" />
+  </nord-modal>
 </template>
 
 <style scoped>
