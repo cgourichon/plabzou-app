97f6f38dabf10d44ec0a76f57e5e2aabc6a12b66
Marjo <marjorie.gestin2020@campus-eni.fr>
2023-10-20 17:54:12 +0200

    add view

--- src/views/Communication/RequestsView.vue
+++ src/views/Communication/RequestsView.vue
@@ -1,11 +1,42 @@
 <script setup>
+import {useAuthStore} from "@/stores/auth.store";
+import RequestItem from "@/components/Request/RequestItem.vue";
 
+const authStore = useAuthStore();
 </script>
 
 <template>
-
+    <div class="container">
+        <template v-for="request in authStore.authenticatedUser.teacher.requests" :key="request.id">
+            <div class="card">
+                <RequestItem :request="request"/>
+            </div>
+        </template>
+    </div>
 </template>
 
 <style scoped>
 
+@media (min-width: 1400px) {
+    .container {
+        grid-template-columns: repeat(3, 1fr);
+        max-width: none;
+    }
+}
+
+@media (min-width: 696px) and (max-width: 1400px) {
+    .container {
+        grid-template-columns: repeat(2, 1fr);
+        max-width: none;
+    }
+}
+
+.container {
+    display: grid;
+    grid-gap: 24px;
+}
+
+.card {
+  height: 100%;
+}
 </style>
