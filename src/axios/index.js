7a408540fa7854639805b42546d9b11ce9ca0949
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2023-10-22 22:08:30 +0200

    set prod api url

--- src/axios/index.js
+++ src/axios/index.js
@@ -4,7 +4,7 @@ import {useApplicationStore} from "@/stores/application.store";
 import router from "@/router";
 
 const axiosClient = axios.create({
-    baseURL: 'http://plabzou-api.test/api'
+    baseURL: 'https://plabzou-api.mitermitek.fr/api'
 })
 
 axiosClient.interceptors.request.use(config => {
